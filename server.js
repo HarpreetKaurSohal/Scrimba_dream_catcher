bda35d7baceeed5ddf59aae982d892ca54da6319
HarpreetKaurSohal <preet3089996@gmail.com>
2026-07-15 16:16:33 +0530

    updated changes to make the app working, using groq API

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+import 'dotenv/config';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import helmet from 'helmet'
 import { initDatabase } from './config/database-init.js';
 import dreamsRouter from './routes/dreams.js';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
